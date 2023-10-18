9bb421171e679311e29235f3dde78e75759d1b04
codescalper <msfunbook@gmail.com>
2023-10-18 19:10:19 +0530

    Completed project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use csv; //csv crate in rust
+use std::error::Error; //to print out std error message
+
+fn read_from_csv(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = read_from_csv("./Sample.csv") {
+        eprintln!("{}", e); //* eprintln! -> only for error and progress messages. */
+    }
 }
